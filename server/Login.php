ce76afdb49aa4f7ac0cf9013e724a26f4c68d976
AadarshSree <chichusr@gmail.com>
2020-09-23 11:54:55 +0400

    sed bug fix

--- server/Login.php
+++ server/Login.php
@@ -175,7 +175,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	<div class="limiter">
 		<div class="container-login100">
-			<div class="login100-more" style="background-image: url('login_res/images/bg-03git .jpg');">
+			<div class="login100-more" style="background-image: url('login_res/images/bg-03.jpg');">
 			</div>
 
 			<div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
