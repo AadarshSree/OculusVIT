e1bdc9a37a13fa1d01075a72ba89123b4e1d1e35
gitman69420 <s.mugundan2000@gmail.com>
2020-10-25 14:19:14 +0530

    violation table added, ajax request to update the table, console will log every violation taking place

--- server/violationUpdate.php
+++ server/violationUpdate.php
@@ -4,6 +4,11 @@
     session_start();
     $username = $_SESSION['username'];
     
+    //getting sid from students table
+    $sid_data = mysqli_query($sql_api, "SELECT sid FROM students WHERE username='$username'");
+    $student_id = mysqli_fetch_assoc($sid_data);
+    $sid = $student_id['sid'];
+
     //getting examid of exam being attended 
     $eid_data = mysqli_query($sql_api,"SELECT eid FROM history WHERE status='ongoing' AND username='$username'");
     $examid = mysqli_fetch_assoc($eid_data);
@@ -18,9 +23,17 @@
     $r_date = $date->format('Y-m-d H:i:s');
     //$array[] = array("master", "Idk some exam" ,$date);
 
+    //$time = time();
+    if(mysqli_query($sql_api, "INSERT INTO violations VALUES(NULL, '$sid', '$username', '$eid', NULL)")){
+        
+    }
+    else{
+        die("Unable to update violation, please reload the page and try again");
+    }
+
     $str = "Violation Reported! -> ".$username."/".$ename."/".$r_date;
 
-    //INSERT INTO violations(username, examname, date) VALUES ('$username','$eName','$date');
+    //INSERT INTO violations(username, examname, date) VALUES ('$username','$eName','$r_date');
 
     echo $str;
 ?>
