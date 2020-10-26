71d1f8b72a423b21dfc4de76e01c975585438078
gitman69420 <s.mugundan2000@gmail.com>
2020-10-26 10:28:27 +0530

    no msg

--- server/dashboard.php
+++ server/dashboard.php
@@ -63,7 +63,7 @@ if (! (isset($_SESSION['username'])   || isset($_SESSION['authID']) ) && $_SESSI
 <nav class="navbar navbar-default title1">
   <div class="container-fluid">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="true">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
